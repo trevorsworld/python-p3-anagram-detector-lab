6fe57f4acc54529db516f100c14d1b6a05d8db75
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-24 07:46:04 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self._word = word.lower()
+
+    def match(self, anagrams):
+        word_chars_main = [char for char in self._word]
+        matching_words = []
+        for word in anagrams:
+            word_chars = [char for char in word]
+            match_check = sorted(word_chars) == sorted(word_chars_main)
+            if match_check:
+                matching_words.append(word)
+        return (matching_words)
